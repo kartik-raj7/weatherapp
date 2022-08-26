1b079793645ef742150be9bb60cfff059124b4d8
kartik-raj7 <kartikeyrajgupta007@gmail.com>
2022-08-26 21:56:44 +0530

    footer updated

--- src/Footer.js
+++ src/Footer.js
@@ -1,11 +1,17 @@
 import React from 'react'
 function Footer(){
     return(
-     <div className='Footer'>
-        <div className='item'>
-        <h2>Made with React</h2>
-        </div>
-        </div>
+        <footer id="Footer">
+        <div class="container-fluid">
+          <a href ="https://www.linkedin.com/in/kartikey-gupta20/"><i class="social-icon fab fa-linkedin fa-2x"></i></a>
+          <a href="https://twitter.com/KartikeyRaj007"><i class="social-icon fab fa-twitter fa-2x"></i></a>
+          <a href="https://www.instagram.com/kartik__raj7/"><i class="social-icon fab fa-instagram fa-2x"></i></a>
+          <a href="mailto: kartikeyrajgupta007@gmail.com"><i class="social-icon fas fa-envelope fa-2x" src=""></i></a>
+          <div class="ending">
+            <p>Made By Kartikey With ❤️</p>
+          </div>
+              </div>
+      </footer>
     )
 }
 export default Footer;
